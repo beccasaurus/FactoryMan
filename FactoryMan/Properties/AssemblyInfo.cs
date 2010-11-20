2101c15e7e88db87b88c739a973fb2bc925b1bfa
remi <remi@remitaylor.com>
2010-11-20 01:40:22 -0700

    Updated AssemblyInfo.  Removed attributes we're not using.

--- FactoryMan/Properties/AssemblyInfo.cs
+++ FactoryMan/Properties/AssemblyInfo.cs
@@ -2,17 +2,10 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("FactoryMan")]
-[assembly: AssemblyDescription("Simple .NET Factories for focused and readable tests.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("remi Taylor")]
-[assembly: AssemblyProduct("FactoryMan")]
-[assembly: AssemblyCopyright("Copyright ©  2010")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-[assembly: ComVisible(false)]
-[assembly: Guid("900d18b9-bf09-4531-9c43-c05d359d4d98")]
-
 [assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: AssemblyTitle("FactoryMan")]
+[assembly: AssemblyProduct("FactoryMan")]
+[assembly: AssemblyDescription("Simple .NET Factories for focused and readable tests.")]
+[assembly: AssemblyCompany("remi Taylor")]
+[assembly: AssemblyCopyright("Copyright © 2010")]
